22c2d1f8eab3a6942ce42cd022f1fff29d1bfcda
vien3440 <viendev@gmail.com>
2017-08-09 15:17:59 +0200

    ajout des methodes deck de session

--- main.js
+++ main.js
@@ -1,14 +1,55 @@
 
 var express = require('express');
 var app = express();
+var session = require('cookie-session'); // Charge le middleware de sessions
+var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
+
+/* On utilise les sessions */
+app.use(session({secret: 'todotopsecret'}))
+
+
+
+
+
+
+        .get('/add/session/deck', function (req, res) {
+            /* S'il n'y a pas de deckList dans la session,
+             on en crée une vide sous forme d'array */
+
+            if (typeof (req.session.deckList) == 'undefined') {
+                req.session.deckList = [];
+            }
+
+            res.redirect('/');
+        })
+
+        /* On ajoute une catre à la list */
+        .get('/session/newDeck/ajouter/carte', function (req, res) {
+
+            req.session.deckList.push('pikatchu');
+
+            res.redirect('/');
+        })
+
+
+
+        .get('/session/Deck/supr', function (req, res) {
+            delete req.session.deckList;
+
+            res.redirect('/');
+        });
+        
+        
+        
 app.get('/', function (req, res) {
-  
+    console.log(req.session.deckList);
 //  Pour servir des fichiers statiques
-  app.use(express.static('web'));
-  // affiche la vue  
-  res.sendFile(__dirname +'/views/home.html');
-  
+    app.use(express.static('web'));
+    // affiche la vue  
+    res.sendFile(__dirname + '/views/home.html');
+
 });
 
 app.listen(8000, function () {
---
+++ web/angular/sessionNewDeck.js
@@ -0,0 +1,7 @@
+ angular.module('app', [])
+                    .controller('form', function ($scope, $http) {
+                        
+                        
+                    });
+
+
